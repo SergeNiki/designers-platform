561621397d9c61620665a2f8396c6bc0b5ad7702
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-07-19 22:12:18 +0500

    add opening a window to create subscription

--- src/pages/Profile/Subscriptions/Subscriptions.tsx
+++ src/pages/Profile/Subscriptions/Subscriptions.tsx
@@ -1,15 +1,17 @@
 import { connect } from "react-redux";
 import { StateType } from "../../../redux/redux-store";
-import { ISubscriptionsState, SubData } from "../../../types/subscriptions";
+import { SubData } from "../../../types/subscriptions";
 import { getSubscriptions } from "../../../redux/subscriptions-reducer"
 import classes from "./Subscriptions.module.css";
 import SubscriptionsCard from "./SubscriptionsCard/SubscriptionsCard";
 import { useEffect, useState } from "react";
+import SubCreateWindow from "./SubCreateWindow/SubCreateWindow";
 
 type SubscriptionsProps = {
   //from State
   subscriptionsCount: number
   subscriptionsArr: Array<SubData>
+  isFetching: boolean
   getSubscriptions(id: number): void
 
   //from parent
@@ -25,17 +27,25 @@ const Subscriptions = (props: SubscriptionsProps) => {
 
   const [isCreateWindow, setIsCreateWindow] = useState<boolean>(false)
 
-  if(props.subscriptionsCount == 0 && !props.isOwner) {
-    return <h1>У данного пользователя отсутствуют платные подписки</h1>
+  const openSubCreateWindow = (): void => {
+    document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
+    setIsCreateWindow(true);
   }
 
   const subscriptions = props.subscriptionsArr.map((sub: SubData) => <SubscriptionsCard key={sub.id} {...sub} />)
 
+  if(props.isFetching) {
+    return <p>Loading...</p>
+  }
+  if(props.subscriptionsCount == 0 && !props.isOwner) {
+    return <h1>У данного пользователя отсутствуют платные подписки</h1>
+  }
+
   return (
     <div className={classes.subscriptions_wrap}>
-      {isCreateWindow}
+      {isCreateWindow && <SubCreateWindow setIsCreateWindow={setIsCreateWindow} />}
       <div className={classes.subscriptions}>
-        {props.isOwner && <div className={classes.add_sub_btn} onClick={() => setIsCreateWindow(true)} >
+        {props.isOwner && <div className={classes.add_sub_btn} onClick={openSubCreateWindow} >
           добавить
         </div>}
         {subscriptions}
@@ -46,7 +56,8 @@ const Subscriptions = (props: SubscriptionsProps) => {
 
 let mapStateToProps = (state: StateType) => ({
   subscriptionsCount: state.subscriptionsData.count,
-  subscriptionsArr: state.subscriptionsData.results
+  subscriptionsArr: state.subscriptionsData.results,
+  isFetching: state.subscriptionsData.isFetching
 })
 
 export default connect(mapStateToProps, {
