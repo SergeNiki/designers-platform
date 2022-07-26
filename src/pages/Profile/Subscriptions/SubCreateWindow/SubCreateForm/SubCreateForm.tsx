401699d61ac79a7ec9d3d46d9a1e5bfbad538226
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-07-26 14:25:37 +0300

    add preview for cover nd refactoring

--- src/pages/Profile/Subscriptions/SubCreateWindow/SubCreateForm/SubCreateForm.tsx
+++ src/pages/Profile/Subscriptions/SubCreateWindow/SubCreateForm/SubCreateForm.tsx
@@ -2,9 +2,9 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import classes from './SubCreateForm.module.css';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-import { maxHeaderSize } from 'http';
 import Button from '../../../../../components/Button/Button';
 import { SubscriptionData } from '../../../../../types/subscriptions';
+import { useState } from 'react';
 
 type SubCreateFormProps = {
   creatingSubscription(data: SubscriptionData): void
@@ -18,63 +18,77 @@ type SubForm = {
 };
 
 const SubCreateForm = (props: SubCreateFormProps) => {
-  const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
+  const [imageFile, setImageFile] = useState<FormData>()
+  const [coverPreview, setCoverPreview] = useState<string>('')
 
   const validationSchema = Yup.object().shape({
     sub_name: Yup.string()
       .required('У подписки должно быть название!')
       .min(3, 'Название подписки не может содержать менее 3 символов')
       .max(20, 'Название подписки не может содержать более 20 символов'),
-    sub_image: Yup.mixed()
-      .test('required', "You need to provide a file", (value) =>{
-        return value && value.length
-      } )
-      .test("fileSize", "The file is too large", (value, context) => {
-        return value && value[0] && value[0].size <= 200000;
-      })
-      .test("type", "We only support jpeg", function (value) {
-        return value && value[0] && value[0].type === "image/jpeg";
-      }),
     sub_description: Yup.string()
       .required('У подписки должно быть описание!')
       .min(3, 'Описание не может содержать менее 3 символов!')
       .max(500, 'Описание не может содержать более 500 символов!'),
     sub_price: Yup.string()
-      .required('Стоимость подписки не может содержать менее одного символа!')
+      .required('Это поле не может быть пустым!')
       .matches(/^[0-9]+$/, 'Стоимость подписки может содержать только числа!')
       .test(
-        '',
-        'Максимальная стоимость не может превышать 99999 рублей!',
-        (price) => Number(price) < 100000
+        'min',
+        'Минимальная стоимость подписки начинается с 15 рублей!',
+        (price) => Number(price) >= 15
+      )
+      .test(
+        'max',
+        'Максимальная стоимость не может превышать 100000 рублей!',
+        (price) => Number(price) <= 100000
       ),
   });
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     reset,
   } = useForm<SubForm>({
+    mode: "onChange",
     resolver: yupResolver(validationSchema),
   });
   const onSubmit: SubmitHandler<SubForm> = (data) => {
-    const formData = new FormData();
-    formData.append("iamge", data.sub_image)
-    // props.creatingSubscription({
-    //   description: data.sub_description,
-    //   image: data.sub_image,
-    //   name: data.sub_name,
-    //   price: data.sub_price,
-    //   price_currency: "RUB"})
-    console.log(data);
+    console.log(imageFile!.get('image'))
+    props.creatingSubscription({
+      description: data.sub_description,
+      image: imageFile!.get('image'),
+      name: data.sub_name,
+      price: data.sub_price,
+      price_currency: "RUB"})
     reset();
   };
 
   const uploadImage = (e: any) => {
     e.preventDefault()
+    setCoverPreview('')
     let fileInput = document.getElementById('sub_image')
     fileInput?.click()
   }
+
+  const handleImageFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files?.length) {
+      const file = e.currentTarget.files[0];
+      if (file.type == 'image/jpeg') {
+        const reader = new FileReader();
+        reader.readAsDataURL(file)
+        reader.onloadend = function(e) {
+          setCoverPreview(String(this.result))
+        }
+        const formData = new FormData();
+        formData.append("image", file)
+        setImageFile(formData);
+      } else {
+        alert('Неподходящий тип или формат файла!');
+      }
+    }
+  } 
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={classes.sub_form}>
@@ -89,21 +103,22 @@ const SubCreateForm = (props: SubCreateFormProps) => {
         <div className={classes.error_message}>{errors.sub_name?.message}</div>
       </div>
       <div className={classes.sub_image + ' ' + classes.form_item}>
-        <label htmlFor="sub_image">Обложка подписки</label>
-        <input style={{display: 'none'}} id="sub_image" type="file" {...register('sub_image')} />
+        <label>Обложка подписки</label>
+        <input style={{display: 'none'}} id="sub_image" type="file" {...register('sub_image')} onChange={handleImageFile} />
+        {coverPreview !== '' ? <img src={coverPreview} alt='обложка' className={classes.cover_preview} /> : <></>}
         <Button 
           isDisabled={false}
           backgroundColor={'#f0f0f0'}
           hoverBackgroundColor={'#67c598'}
           buttonSize='medium'
-          handleClick={uploadImage} >Добавить обложку</Button>
+          handleClick={uploadImage} >{coverPreview ? 'Изменить' : 'Добавить обложку'}
+        </Button>
         {/* <div className={classes.error_message}>{errors.sub_image?.message}</div> */}
       </div>
       <div className={classes.sub_description + ' ' + classes.form_item}>
         <label htmlFor="sub_description">Описание подписки</label>
         <textarea
           id="sub_description"
-          // type="text"
           placeholder="Введите описание"
           {...register('sub_description')}
         />
@@ -119,10 +134,9 @@ const SubCreateForm = (props: SubCreateFormProps) => {
         />
         <div className={classes.error_message}>{errors.sub_price?.message}</div>
       </div>
-      {/* <button disabled={!errors} type="submit">Создать</button> */}
       <div className={classes.btn_wrap}>
         <Button
-          isDisabled={false}
+          isDisabled={!isValid || !coverPreview}
           backgroundColor={'#6DEFC0'}
           hoverBackgroundColor={'#67c598'}
           buttonSize='medium'
