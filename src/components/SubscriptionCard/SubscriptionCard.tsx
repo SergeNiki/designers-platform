01a013f8d03afaa87ea476c4fad7c801980309c9
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-07-28 14:53:06 +0300

    relocation and adding del/sub subscription

---
+++ src/components/SubscriptionCard/SubscriptionCard.tsx
@@ -0,0 +1,56 @@
+import { StateType } from '../../redux/redux-store';
+import { SubData } from '../../types/subscriptions';
+import classes from './SubscriptionCard.module.css';
+
+type SubscriptionCardProps = SubData & {
+  isOwner: boolean;
+  deleteSub(subId: number): void;
+  subscribe(subId: number): void;
+};
+
+const SubscriptionCard = (props: SubscriptionCardProps) => {
+  const getSubButtons = (): JSX.Element => {
+    if (props.isOwner) {
+      let el = (
+        <div className={classes.button_del_wrap} >
+          <div className={classes.price_wrap}>
+            {props.price} {props.price_currency}
+          </div>
+          <button
+            className={classes.unsub_button}
+            onClick={() => props.deleteSub(props.id)}
+          >
+            Удалить
+          </button>
+        </div>
+      );
+      return el;
+    } else if (props.is_subscribed) {
+      return <button className={classes.unsub_button}>Отменить</button>;
+    } else {
+      return (
+        <button
+          className={classes.sub_button}
+          onClick={() => props.subscribe(props.id)}
+        >
+          {props.price} {props.price_currency}
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div className={classes.sub_wrap} style={{backgroundImage: `url(${props.image})`}} >
+      <div className={classes.sub_name_wrap}>
+        <h3>{props.name}</h3>
+      </div>
+      <div></div>
+      <div className={classes.sub_description}>{props.description}</div>
+      {getSubButtons()}
+    </div>
+  );
+};
+
+let mapSateToProps = (state: StateType) => ({});
+
+export default SubscriptionCard;
