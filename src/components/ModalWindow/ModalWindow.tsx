2aca8952756855f49a83697f320255ef726fc549
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-04 14:12:03 +0300

    change the modal window for ModalHeader

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,10 +1,13 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect } from 'react';
+import React, { CSSProperties, useEffect } from 'react';
+import ModalHeader from './ModalHeader/ModalHeader';
 import classes from './ModalWindow.module.css';
 
 type ModalWindowProps = {
   children: React.ReactNode;
+  header?: React.ReactNode;
+  styles?: CSSProperties
   closeWindow(value: false): void;
 };
 
@@ -16,12 +19,23 @@ const ModalWindow = (props: ModalWindowProps) => {
     };
   }, []);
   return (
-    <div className={classes.modal_wrap} onClick={() => props.closeWindow(false)} >
-      <div className={classes.modal_window} onClick={(e) => e.stopPropagation()} >
+    <div
+      className={classes.modal_wrap}
+      onClick={() => props.closeWindow(false)}
+    >
+      <div
+        className={classes.modal_window}
+        style={props?.styles}
+        onClick={(e: React.MouseEvent) => e.stopPropagation()}
+      >
+        <ModalHeader>{props.header}</ModalHeader>
         <div className={classes.close_btn}>
-          <FontAwesomeIcon icon={faXmark} onClick={() => props.closeWindow(false)} />
+          <FontAwesomeIcon
+            icon={faXmark}
+            onClick={() => props.closeWindow(false)}
+          />
         </div>
-        {props.children}
+        <div className={classes.modal_content}>{props.children}</div>
       </div>
     </div>
   );
