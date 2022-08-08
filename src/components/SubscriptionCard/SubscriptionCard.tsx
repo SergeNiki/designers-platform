d399e680d77662b064f7e10e0edeb2b694cf9fe2
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-08 14:54:24 +0300

    restyling sub. cards and add the ability to edit subs

--- src/components/SubscriptionCard/SubscriptionCard.tsx
+++ src/components/SubscriptionCard/SubscriptionCard.tsx
@@ -1,58 +1,78 @@
-import { StateType } from '../../redux/redux-store';
+import { useEffect, useState } from 'react';
 import { SubData } from '../../types/subscriptions';
+import SubCreateWindow from '../SubCreateWindow/SubCreateWindow';
+import SubEditForm from '../SubCreateWindow/SubEditForm/SubEditForm';
+import SubButtons from './SubButtons/SubButtons';
 import classes from './SubscriptionCard.module.css';
 
 type SubscriptionCardProps = SubData & {
   isOwner: boolean;
+  isFetching: boolean;
   deleteSub(subId: number): void;
   subOrUnsub(subId: number, type: 'sub' | 'unsub'): void;
 };
 
 const SubscriptionCard = (props: SubscriptionCardProps) => {
-  const getSubButtons = (): JSX.Element => {
-    if (props.isOwner) {
-      let el = (
-        <div className={classes.button_del_wrap} >
-          <div className={classes.price_wrap}>
-            {props.price} {props.price_currency}
-          </div>
-          <button
-            className={classes.unsub_button}
-            onClick={() => props.deleteSub(props.id)}
-          >
-            Удалить
-          </button>
-        </div>
-      );
-      return el;
-    } else if (props.is_subscribed) {
-      return <button className={classes.unsub_button} 
-      onClick={() => props.subOrUnsub(props.id, 'unsub')} 
-      >Отменить</button>;
-    } else {
-      return (
-        <button
-          className={classes.sub_button}
-          onClick={() => props.subOrUnsub(props.id, 'sub')}
-        >
-          {props.price} {props.price_currency}
-        </button>
-      );
+  const [isEditWindow, setIsEditWindow] = useState<boolean>(false);
+  const [isTextWrap, setIsTextWrap] = useState<boolean>(false);
+
+  useEffect(() => {
+    for (let word of props.name.split(' ')) {
+      if (
+        word.length > 24 &&
+        (window.innerWidth > 940 || window.innerWidth < 768)
+      ) {
+        setIsTextWrap(true);
+      }
     }
-  };
+  }, []);
 
   return (
-    <div className={classes.sub_wrap} style={{backgroundImage: `url(${props.image})`}} >
-      <div className={classes.sub_name_wrap}>
-        <h3>{props.name}</h3>
+    <>
+      {isEditWindow && (
+        <SubCreateWindow setIsCreateWindow={setIsEditWindow}>
+          <SubEditForm
+            subId={props.id}
+            closeWindow={setIsEditWindow}
+            sub_name={props.name}
+            sub_description={props.description}
+            sub_price={props.price}
+            sub_cover_preview={props.image}
+          />
+        </SubCreateWindow>
+      )}
+      <div
+        className={classes.sub_wrap}
+        style={{ backgroundImage: `url(${props.image})` }}
+      >
+        <div className={classes.sub_header}>
+          <div className={classes.price_wrap}>
+            {props.price.split('.')[0]} {props.price_currency}
+          </div>
+          <div
+            className={classes.sub_name_wrap}
+            style={{ wordBreak: isTextWrap ? 'break-all' : 'normal' }}
+          >
+            {props.name}
+          </div>
+        </div>
+
+        <div></div>
+        <div className={classes.sub_description}>{props.description}</div>
+        <SubButtons
+          isOwner={props.isOwner}
+          isFetching={props.isFetching}
+          id={props.id}
+          price={props.price}
+          price_currency={props.price_currency}
+          is_subscribed={props.is_subscribed}
+          deleteSub={props.deleteSub}
+          subOrUnsub={props.subOrUnsub}
+          openEditWindow={setIsEditWindow}
+        />
       </div>
-      <div></div>
-      <div className={classes.sub_description}>{props.description}</div>
-      {getSubButtons()}
-    </div>
+    </>
   );
 };
-
-let mapSateToProps = (state: StateType) => ({});
 
 export default SubscriptionCard;
