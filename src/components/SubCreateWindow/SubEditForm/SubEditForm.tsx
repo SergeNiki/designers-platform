115b04855f2885c765e7571163b9833cb5aa6ec2
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-09 09:34:34 +0300

    sending data to download a subscription and refact.

--- src/components/SubCreateWindow/SubEditForm/SubEditForm.tsx
+++ src/components/SubCreateWindow/SubEditForm/SubEditForm.tsx
@@ -1,102 +1,107 @@
-import { connect } from "react-redux"
+import { connect } from 'react-redux';
 import * as Yup from 'yup';
-import { StateType } from "../../../redux/redux-store"
-import {editSubscription} from '../../../redux/subscriptions-reducer'
-import { checkImage } from '../../../redux/image-reducer';
-import { SubscriptionData } from "../../../types/subscriptions"
-import SubCoverDownload from "../SubCoverDownload/SubCoverDownload";
-import { FormElement } from "../../../types/form";
-import Form from "../../Form/Form";
+import { StateType } from '../../../redux/redux-store';
+import { editSubscription } from '../../../redux/subscriptions-reducer';
+import { checkImage, clearImageState } from '../../../redux/image-reducer';
+import SubCoverDownload from '../SubCoverDownload/SubCoverDownload';
+import { FormElement } from '../../../types/form';
+import Form from '../../Form/Form';
+import { useEffect } from 'react';
+import { SubEditData } from '../../../types/subscriptions';
 
 type SubEditFormProps = {
-    imageFile: File | null;
-    coverPreview: string;
-    checkImage(
-        event: React.ChangeEvent<HTMLInputElement>,
-        maxSize: number,
-        isSuccess: React.Dispatch<React.SetStateAction<boolean>>
-      ): void;
-    editSubscription(subId: number, data: SubscriptionData): void
+  imageFile: File | null;
+  coverPreview: string;
+  clearImageState(): void;
+  checkImage(
+    event: React.ChangeEvent<HTMLInputElement>,
+    maxSize: number,
+    isSuccess: React.Dispatch<React.SetStateAction<boolean>>
+  ): void;
+  editSubscription(subId: number, data: SubEditData): void;
 
-    subId: number
-    sub_name: string
-    sub_description: string
-    sub_cover_preview: string
-    sub_price: string
-    closeWindow(value: false): void
-}
+  subId: number;
+  subName: string;
+  subDescription: string;
+  subCoverPreview: string;
+  subPrice: string;
+  closeWindow(value: false): void;
+};
 
 type SubForm = {
-    sub_name: string;
-    sub_description: string;
-  };
+  subName: string;
+  subDescription: string;
+};
 
 const SubEditForm = (props: SubEditFormProps) => {
-    const validationSchema = Yup.object().shape({
-        sub_name: Yup.string()
-          .required('У подписки должно быть название!')
-          .min(3, 'Название подписки не может содержать менее 3 символов')
-          .max(40, 'Название подписки не может содержать более 40 символов'),
-        sub_description: Yup.string()
-          .required('У подписки должно быть описание!')
-          .min(3, 'Описание не может содержать менее 3 символов!')
-          .max(2000, 'Описание не может содержать более 2000 символов!')
-      });
+  useEffect(() => {
+    return () => props.clearImageState()
+  }, [])  
+  
+  const validationSchema = Yup.object().shape({
+    subName: Yup.string()
+      .required('У подписки должно быть название!')
+      .min(3, 'Название подписки не может содержать менее 3 символов')
+      .max(40, 'Название подписки не может содержать более 40 символов'),
+    subDescription: Yup.string()
+      .required('У подписки должно быть описание!')
+      .min(3, 'Описание не может содержать менее 3 символов!')
+      .max(2000, 'Описание не может содержать более 2000 символов!'),
+  });
 
-      const onSubmit = (data: SubForm) => {
-        props.editSubscription(
-        props.subId,
-        {
-          description: data.sub_description,
-          image: props.imageFile,
-          name: data.sub_name,
-          price: props.sub_price,
-          price_currency: 'RUB',
-        });
-        props.closeWindow(false);
-      };
+  const onSubmit = (data: SubForm) => {
+    let editBody: SubEditData = {}
+    if(data.subName) editBody.name = data.subName
+    if(data.subDescription) editBody.description = data.subDescription
+    if (props.imageFile) editBody.image = props.imageFile
+    props.editSubscription(props.subId, editBody);
+    props.closeWindow(false);
+  };
 
-      const extraElement = (
-        <SubCoverDownload
-          coverPreview={props.coverPreview ? props.coverPreview : props.sub_cover_preview}
-          checkImage={props.checkImage}
-        />
-      );
+  const extraElement = (
+    <SubCoverDownload
+      coverPreview={props.coverPreview}
+      defaultCoverPreview={props.subCoverPreview}
+      checkImage={props.checkImage}
+    />
+  );
 
-      const formElements: Array<FormElement> = [
-        {
-          tag: 'input',
-          id: 'sub_name',
-          type: 'text',
-          label: 'Название подписки',
-          defaultvalue: props.sub_name,
-          placeholder: 'Введите название',
-        },
-        {
-          tag: 'textarea',
-          id: 'sub_description',
-          label: 'Описание подписки',
-          defaultvalue: props.sub_description,
-          placeholder: 'Введите описание',
-        },
-      ];
+  const formElements: Array<FormElement> = [
+    {
+      tag: 'input',
+      id: 'subName',
+      type: 'text',
+      label: 'Название подписки',
+      defaultvalue: props.subName,
+      placeholder: 'Введите название',
+    },
+    {
+      tag: 'textarea',
+      id: 'subDescription',
+      label: 'Описание подписки',
+      defaultvalue: props.subDescription,
+      placeholder: 'Введите описание',
+    },
+  ];
 
-    return     <Form
-    onSubmit={onSubmit}
-    textBtn="Сохранить"
-    formElements={formElements}
-    validSchema={validationSchema}
-    extraDependence={!props.imageFile}
-    extraElements={[{ index: 1, id: 'sub_image', element: extraElement }]}
-  />
-}
+  return (
+    <Form
+      onSubmit={onSubmit}
+      textBtn="Сохранить"
+      formElements={formElements}
+      validSchema={validationSchema}
+      extraElements={[{ index: 1, id: 'sub_image', element: extraElement }]}
+    />
+  );
+};
 
 let mapSateToProps = (state: StateType) => ({
-    imageFile: state.image.imageFile,
-    coverPreview: state.image.coverPreview,
-})
+  imageFile: state.image.imageFile,
+  coverPreview: state.image.coverPreview,
+});
 
 export default connect(mapSateToProps, {
-    checkImage,
-    editSubscription
-})(SubEditForm)
+  checkImage,
+  editSubscription,
+  clearImageState
+})(SubEditForm);
