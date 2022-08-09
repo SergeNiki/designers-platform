751a1049fc4f0330d09177cebbe60cb43a3436e5
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-09 09:33:38 +0300

    add headers for edit sub request

--- src/services/SubscriptionsService.ts
+++ src/services/SubscriptionsService.ts
@@ -1,20 +1,24 @@
 import { AxiosResponse } from "axios";
 import $api from "../http";
-import { GetSubscriptionsResponse, СreatingSubscriptionResponse, SubscriptionData } from "../types/subscriptions";
+import { GetSubscriptionsResponse, SubData, SubCreateData, SubEditData } from "../types/subscriptions";
 
 class SubscriptionsService {
     static async getSubscriptions(userId: number): Promise<AxiosResponse<GetSubscriptionsResponse>> {
         return $api.get<GetSubscriptionsResponse>(`api/v1/users/${userId}/subscription-types/`)
     }
-    static async creatingSubscription(data: SubscriptionData): Promise<AxiosResponse<СreatingSubscriptionResponse>> {
-        return $api.post<СreatingSubscriptionResponse>(`api/v1/subscription-types/`, {...data}, {
+    static async creatingSubscription(data: SubCreateData): Promise<AxiosResponse<SubData>> {
+        return $api.post<SubData>(`api/v1/subscription-types/`, {...data}, {
             headers: {
               'Content-type' : 'multipart/form-data'
             }
           })
     }
-    static async editSubscription(subId: number, data: SubscriptionData): Promise<AxiosResponse<СreatingSubscriptionResponse>> {
-        return $api.patch<СreatingSubscriptionResponse>(`api/v1/subscription-types/${subId}/`, {...data})
+    static async editSubscription(subId: number, data: SubEditData): Promise<AxiosResponse<SubData>> {
+        return $api.patch<SubData>(`api/v1/subscription-types/${subId}/`, {...data}, {
+            headers: {
+              'Content-type' : 'multipart/form-data'
+            }
+          })
     }
     static async deleteSubscription(subId: number): Promise<AxiosResponse> {
         return $api.delete(`api/v1/subscription-types/${subId}/`)
