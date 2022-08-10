2d97ffd7a8eb147a193485bf2669ff32a23fffb6
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-10 14:54:04 +0300

    refactoring and add a tooltip to profile menu

--- src/pages/Profile/Menu/ProfileMenu.tsx
+++ src/pages/Profile/Menu/ProfileMenu.tsx
@@ -1,39 +1,91 @@
-import React from "react";
-import classes from "./ProfileMenu.module.css";
+import {
+  faFolderOpen,
+  faImages,
+  faPaste,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import Tooltips from '../../../components/Tooltips/Tooltips';
+import classes from './ProfileMenu.module.css';
 
 type ProfileMenuProps = {
-  contentType: "posts" | "sub_types" | "albums";
-  setContentType(value: "posts" | "sub_types" | "albums"): void;
+  contentType: 'посты' | 'виды подписок' | 'альбомы';
+  setContentType(value: 'посты' | 'виды подписок' | 'альбомы'): void;
 };
 
 const ProfileMenu: React.FC<ProfileMenuProps> = (props) => {
+  const [tooltipText, setTooltipText] = useState<string>('');
+  const [leftValueActiveBar, setLeftValueActiveBar] = useState<string>('');
+
+  useEffect(() => {
+    if (props.contentType == 'виды подписок') setLeftValueActiveBar('0');
+    else if (props.contentType == 'альбомы')
+      setLeftValueActiveBar('calc((100% / 3) * 2)');
+    else setLeftValueActiveBar('calc(100% / 3)');
+  }, [props.contentType]);
+
+  const showTooltip = (event: React.MouseEvent<HTMLDivElement>) => {
+    let text = event.currentTarget.getAttribute(
+      'datatype'
+    ) as typeof props.contentType;
+    setTooltipText(text);
+  };
+  const hideTooltip = (event: React.MouseEvent<HTMLDivElement>) => {
+    setTooltipText('');
+  };
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    let text = event.currentTarget.getAttribute(
+      'datatype'
+    ) as typeof props.contentType;
+    props.setContentType(text);
+    setTooltipText('');
+  };
+
+  const elementsData: Array<{
+    datatype: typeof props.contentType;
+    icon: IconDefinition;
+  }> = [
+    {
+      datatype: 'виды подписок',
+      icon: faPaste,
+    },
+    {
+      datatype: 'посты',
+      icon: faImages,
+    },
+    {
+      datatype: 'альбомы',
+      icon: faFolderOpen,
+    },
+  ];
+
+  const elements = elementsData.map((element) => {
+    return (
+      <div
+        datatype={element.datatype}
+        onMouseOver={showTooltip}
+        onMouseOut={hideTooltip}
+        onClick={handleClick}
+        className={`${classes.menu_item} ${
+          props.contentType == element.datatype && classes.activeMenu
+        }`}
+      >
+        <FontAwesomeIcon icon={element.icon} />
+        {tooltipText == element.datatype && (
+          <Tooltips orientation="vertical">{tooltipText}</Tooltips>
+        )}
+      </div>
+    );
+  });
 
   return (
     <div className={classes.menu_wrap}>
-      <div
-        onClick={() => props.setContentType("sub_types")}
-        className={`${classes.menu_item} ${
-          props.contentType == "sub_types" && classes.activeMenu
-        }`}
-      >
-        виды подписки
-      </div>
-      <div
-        onClick={() => props.setContentType("posts")}
-        className={`${classes.menu_item} ${
-          props.contentType == "posts" && classes.activeMenu
-        }`}
-      >
-        посты
-      </div>
-      <div
-        onClick={() => props.setContentType("albums")}
-        className={`${classes.menu_item} ${
-          props.contentType == "albums" && classes.activeMenu
-        }`}
-      >
-        альбомы
-      </div>
+      <span
+        className={classes.active_bar}
+        style={{ left: leftValueActiveBar }}
+      ></span>
+      {elements}
     </div>
   );
 };
