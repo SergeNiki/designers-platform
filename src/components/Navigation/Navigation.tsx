c67a82170b146cf8a42a562ce1cc8c6921a771a4
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-10 14:50:10 +0300

    add the tooltip to Navigation and refactoring

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,39 +1,103 @@
-import React from 'react';
+import React, { MouseEvent, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import classes from './Navigation.module.css'
+import classes from './Navigation.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouseChimney, faImages, faSquarePlus, faUser, faUserGear } from "@fortawesome/free-solid-svg-icons";
-import { faComments, faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import {
+  faAddressBook,
+  faHouseChimney,
+  faUser,
+  faComments,
+  faThumbsUp,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
-import { toggleLoginTC } from "./../../redux/auth-reducer";
+import { toggleLoginTC } from './../../redux/auth-reducer';
 import { StateType } from '../../redux/redux-store';
+import Tooltips from '../Tooltips/Tooltips';
 
 type NavigationProps = {
-    user_id: number | null
-    isAuth: boolean
-    toggleLoginTC(): void
-}
+  user_id: number | null;
+  isAuth: boolean;
+  toggleLoginTC(): void;
+};
 
 const Navigation: React.FC<NavigationProps> = (props) => {
+  const [tooltipText, setTooltipText] = useState<string>('');
 
+  const navElementsData: Array<{
+    path: string;
+    datatype: string;
+    icon: IconDefinition;
+  }> = [
+    {
+      path: '/main',
+      datatype: 'главная',
+      icon: faHouseChimney,
+    },
+    {
+      path: '/subscriptions',
+      datatype: 'подписки',
+      icon: faAddressBook,
+    },
+    {
+      path: `/profile/id${props.user_id}`,
+      datatype: 'профиль',
+      icon: faUser,
+    },
+    {
+      path: '/messages',
+      datatype: 'сообщения',
+      icon: faComments,
+    },
+    {
+      path: '/likes',
+      datatype: 'понравившееся',
+      icon: faThumbsUp,
+    },
+  ];
+
+  const showTooltip = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    let text = String(event.currentTarget.getAttribute('datatype'));
+    setTooltipText(text);
+  };
+  const hideTooltip = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    setTooltipText('');
+  };
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    setTooltipText('');
+  };
+
+  const navElements = navElementsData.map((element) => {
     return (
-        <nav>
-            <div className={classes.nav_wrap}>
-                <div className={classes.nav_list}>
-                    <NavLink to={"/main"}><FontAwesomeIcon icon={faHouseChimney} /></NavLink>
-                    <NavLink to={"/subscriptions"}><FontAwesomeIcon icon={faImages} /></NavLink>
-                    {props.isAuth && <NavLink to={`/profile/id${props.user_id}`}><FontAwesomeIcon icon={faUser} /></NavLink>}
-                    <NavLink to={"/messages"}><FontAwesomeIcon icon={faComments} /></NavLink>
-                    <NavLink to={"/"}><FontAwesomeIcon icon={faThumbsUp} /></NavLink>
-                </div>
-            </div>
-        </nav>
-    )
-}
+      <NavLink
+        to={element.path}
+        datatype={element.datatype}
+        onMouseOver={showTooltip}
+        onMouseOut={hideTooltip}
+        onClick={handleClick}
+      >
+        <FontAwesomeIcon icon={element.icon} />
+        {tooltipText == element.datatype && (
+          <Tooltips orientation="horizontal">{tooltipText}</Tooltips>
+        )}
+      </NavLink>
+    );
+  });
+
+  return (
+    <nav>
+      <div className={classes.nav_wrap}>
+        <div className={classes.nav_list}>
+          {navElements}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 let mapStateToProps = (state: StateType) => ({
-    user_id: state.auth.id,
-    isAuth: state.auth.isAuth
-})
+  user_id: state.auth.id,
+  isAuth: state.auth.isAuth,
+});
 
 export default connect(mapStateToProps, { toggleLoginTC })(Navigation);
