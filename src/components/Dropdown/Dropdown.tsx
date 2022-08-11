11728e20f317f8e971012f649f83ce38b707a86c
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-11 13:32:08 +0300

    add the component Dropdown for context menu

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,37 @@
+import { CSSProperties, useEffect, useState } from 'react';
+import classes from './Dropdown.module.css';
+
+type DropdownProps = {
+  children?: React.ReactNode;
+  coordsData: DOMRect;
+  closeDropdownMwnu(): void;
+};
+
+const Dropdown: React.FC<DropdownProps> = (props) => {
+  const [styles, setStyles] = useState<CSSProperties>({});
+  useEffect(() => {
+    let top: string
+    let right: string
+    if (props.coordsData.bottom <= 55) {
+        top = String('55px');
+    } else {
+        top = String(props.coordsData.bottom + 'px');
+    }
+    right = String(window.innerWidth - props.coordsData.right + 'px');
+    setStyles({ top: top, right: right });
+  }, [props.coordsData]);
+  return (
+    <>
+      <div
+        className={classes.dropdown_wrap}
+        onClick={props.closeDropdownMwnu}
+        onTouchStart={props.closeDropdownMwnu}
+      ></div>
+      <div className={classes.dropdown_menu} style={{ ...styles }}>
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+export default Dropdown;
