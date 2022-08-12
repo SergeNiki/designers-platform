982f7218fa5224f9275981f50dda996586ecc9a3
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-08-12 14:42:32 +0300

    add some types for posts

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -1,12 +1,41 @@
 //Previews State
 export interface IPostPreviews {
-    postPreviewsArray: Array<PreviewDataType>
+  // count: number;
+  // next: string | null;
+  // previous: string | null;
+  // results: Array<PostData>;
+  postPreviewsArray: Array<PreviewDataType>
+}
+export type PostData = {
+  id: number
+
 }
 export type PreviewDataType = {
   id: number;
   preview: string;
   likesCount: number;
 };
+export type PostCreateDataReq = {
+  content: Array<PostCreateContentReq>
+  preview: File
+  description: string
+  level_subscription: number
+}
+export type PostCreateContentReq = {
+  file: File
+  queue_mark: number
+}
+export type PostCreateDataRes = Omit<PostCreateDataReq, 'content'>  & {
+  id: number
+  content: Array<PostCreateContentRes>
+  views_count: number
+  likes_count: number
+  created: string
+}
+export type PostCreateContentRes = PostCreateContentReq & {
+  id: number
+  post: number
+}
 
 
 //Previews Action
