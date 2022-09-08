5ec1acbe4711ca1bc4f426a56a1e26a92dfc648e
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-09-08 16:03:20 +0500

    add the ability to open the post creation window

--- src/components/Header/AddContextMenu/AddContextMenu.tsx
+++ src/components/Header/AddContextMenu/AddContextMenu.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import Dropdown from '../../Dropdown/Dropdown';
 import ModalWindow from '../../ModalWindow/ModalWindow';
+import PostCreateWindow from '../../PostCreateForm/PostCreateForm';
 import SubCreateForm from '../../SubCreateWindow/SubCreateForm/SubCreateForm';
 import classes from './AddContextMenu.module.css';
 
@@ -13,30 +14,47 @@ type AddContextMenuProps = {
 
 const AddContextMenu: React.FC<AddContextMenuProps> = (props) => {
   const [isSubCreateWindow, setIsSubCreateWindow] = useState<boolean>(false);
+  const [isPostCreateWindow, setIsPostCreateWindow] = useState<boolean>(false);
 
-  const openSubCreateWindow = () => {
+  const openSubCreateWindow = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
     setIsSubCreateWindow(true);
   };
+  const openPostCreateWindow = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsPostCreateWindow(true);
+  };
 
-  const header = <h2>Создание подписки</h2>;
+  const subHeader = <h2>Создание подписки</h2>;
+  const postHeader = <h2>Создание поста</h2>;
 
   return (
     <>
       {isSubCreateWindow && (
-        <ModalWindow closeWindow={() => setIsSubCreateWindow(false)} header={header}>
+        <ModalWindow
+          closeWindow={() => setIsSubCreateWindow(false)}
+          header={subHeader}>
           <SubCreateForm closeWindow={() => setIsSubCreateWindow(false)} />
+        </ModalWindow>
+      )}
+      {isPostCreateWindow && (
+        <ModalWindow
+          closeWindow={() => setIsPostCreateWindow(false)}
+          header={postHeader}>
+            <PostCreateWindow/>
         </ModalWindow>
       )}
       <Dropdown
         coordsData={props.coordsData}
-        closeDropdownMwnu={() => props.closeMenu(false)}
-      >
-        <div className={classes.create_menu_item}>
-            <FontAwesomeIcon icon={faPlus}/>
+        closeDropdownMwnu={() => props.closeMenu(false)}>
+        <div
+          className={classes.create_menu_item}
+          onClick={openPostCreateWindow}>
+          <FontAwesomeIcon icon={faPlus} />
           <p>Создать пост</p>
         </div>
-        <div className={classes.create_menu_item} onClick={openSubCreateWindow} >
-            <FontAwesomeIcon icon={faCirclePlus}/>
+        <div className={classes.create_menu_item} onClick={openSubCreateWindow}>
+          <FontAwesomeIcon icon={faCirclePlus} />
           <p>Создать подписку</p>
         </div>
       </Dropdown>
