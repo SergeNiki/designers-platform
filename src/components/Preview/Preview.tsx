baeabb256365c866d1970d237bde89eb561d3f26
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-09-19 18:08:53 +0500

    move parts of a component into separate components

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,46 +1,28 @@
-import React, { CSSProperties, useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { StateType } from '../../redux/redux-store';
+import React, { useState } from 'react';
 import { PostPreviewData } from '../../types/postsList';
-import { getSubscriptions } from '../../redux/subscriptions-reducer';
 import classes from './Preview.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faEye } from '@fortawesome/free-regular-svg-icons';
+import PreviewImage from './PreviewImage/PreviewImage';
+import PreviewFooter from './PreviewInfo/PreviewFooter';
+import PreviewHeader from './PreviewInfo/PreviewHeader';
 
 type PreviewProps = {
   previewData: PostPreviewData;
   isOwner: boolean;
-
-  getSubscriptions(userId: number): void;
 };
 
 const Preview: React.FC<PreviewProps> = (props) => {
-  const [headerStyles, setHesderStyles] = useState<CSSProperties>(
-    window.innerWidth > 768
-      ? {
-          top: '-18%',
-        }
-      : { top: '5%' }
-  );
-  const [footerStyles, setFooterStyles] = useState<CSSProperties>(
-    window.innerWidth > 768
-      ? {
-          bottom: '-18%',
-        }
-      : { bottom: '5%' }
+  const [infoTopBottomValues, setInfoTopBottomValues] = useState<number>(
+    window.innerWidth > 768 ? -18 : 5
   );
 
   const mouseEnterHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     if (window.innerWidth > 768) {
-      setHesderStyles({ top: '5%' });
-      setFooterStyles({ bottom: '5%' });
+      setInfoTopBottomValues(5);
     }
   };
   const mouseLeaveHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     if (window.innerWidth > 768) {
-      setHesderStyles({ top: '-18%' });
-      setFooterStyles({ bottom: '-18%' });
+      setInfoTopBottomValues(-18);
     }
   };
 
@@ -49,54 +31,24 @@ const Preview: React.FC<PreviewProps> = (props) => {
       className={classes.preview_wrap}
       onMouseEnter={mouseEnterHandler}
       onMouseLeave={mouseLeaveHandler}>
-      <div className={classes.preview_header} style={headerStyles}>
-        <div className={classes.avatar_wrap}>
-          <NavLink to={`/profile/id${props.previewData.author.id}`}>
-            <img
-              src={props.previewData.author.avatar}
-              className={classes.avatar}
-              alt={props.previewData.author.display_name}
-            />
-          </NavLink>
-        </div>
-        <div className={classes.author_name}>
-          <NavLink to={`/profile/id${props.previewData.author.id}`}>
-            {props.previewData.author.display_name}
-          </NavLink>
-        </div>
-      </div>
-      <div className={classes.preview_footer} style={footerStyles}>
-        <div className={classes.views}>
-          {props.previewData.views_count}
-          <FontAwesomeIcon icon={faEye} />
-        </div>
-        {props.previewData.access ? (
-          <div className={classes.level_sub}>Доступен</div>
-        ) : (
-          <div className={classes.level_sub}>
-            Доступно по подписке: {props.previewData.level_subscription?.name}
-          </div>
-        )}
-        <div className={classes.likes}>
-          {props.previewData.likes_count}
-          <FontAwesomeIcon icon={faHeart} />
-        </div>
-      </div>
-      <NavLink to={'/'}>
-        <img
-          src={props.previewData.preview}
-          alt='image'
-          className={classes.preview_image}
-        />
-      </NavLink>
+      <PreviewHeader
+        topValue={infoTopBottomValues}
+        authorData={props.previewData.author}
+      />
+      <PreviewFooter
+        bottomValue={infoTopBottomValues}
+        viewsCount={props.previewData.views_count}
+        likesCount={props.previewData.likes_count}
+        isAccess={props.previewData.access}
+        subscriptionData={props.previewData.level_subscription}
+      />
+      <PreviewImage
+        isAccess={props.previewData.access}
+        postId={props.previewData.id}
+        previewImage={props.previewData.preview}
+      />
     </div>
   );
 };
 
-let mapSateToProps = (state: StateType) => ({
-  subscriptionName: state.subscriptionsData.results,
-});
-
-export default connect(mapSateToProps, {
-  getSubscriptions,
-})(Preview);
+export default Preview;
